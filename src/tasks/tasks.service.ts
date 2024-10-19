d732943bc7492993f5a33407f06a145e8b58667c
Kamil Cessak <kamil.cessak@outlook.com>
2024-10-19 11:33:23 +0200

    Added error handling to api

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 
@@ -7,27 +12,73 @@ export class TasksService {
   constructor(private prisma: PrismaService) {}
 
   async createTask(data: Prisma.TasksCreateInput) {
-    return this.prisma.tasks.create({
-      data
-    });
+    try {
+      return this.prisma.tasks.create({
+        data,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new BadRequestException(
+          'Invalid data provided for task creation. '
+        );
+      } else {
+        throw new InternalServerErrorException(
+          'An error occurred while creating the task.'
+        );
+      }
+    }
   }
 
-  async deleteTask(id: number){
-    return this.prisma.tasks.delete({ where: {id}});
+  async deleteTask(id: number) {
+    try {
+      return await this.prisma.tasks.delete({
+        where: { id },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(
+            `Task with ID ${id} not found for deletion.`
+          );
+        }
+      }
+      throw new InternalServerErrorException(
+        'An error occurred while deleting the task.'
+      );
+    }
   }
 
-  async updateTask(id: number, data: Prisma.TasksUpdateInput){
-    return this.prisma.tasks.update({
-      where: {id},
-      data,
-    });
+  async updateTask(id: number, data: Prisma.TasksUpdateInput) {
+    try {
+      return await this.prisma.tasks.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(
+            `Task with ID ${id} not found for update.`
+          );
+        }
+      }
+      throw new InternalServerErrorException(
+        'An error occurred while updating the task.'
+      );
+    }
   }
 
-  async getAllTasks(){
-    return this.prisma.tasks.findMany();
+  async getAllTasks() {
+    try {
+      return await this.prisma.tasks.findMany();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'An error occurred while retrieving tasks.'
+      );
+    }
   }
 
-  async getTaskById(id: number){
+  async getTaskById(id: number) {
     const task = await this.prisma.tasks.findUnique({
       where: { id },
     });
